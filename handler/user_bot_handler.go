85c75779e02d38e207b6e088dc91edb9f9a509b1
Alex <467146659@qq.com>
2023-06-08 01:22:03 +0800

    fix: null point

--- handler/user_bot_handler.go
+++ handler/user_bot_handler.go
@@ -17,7 +17,7 @@ type user struct {
 
 type userHandler struct {
 	users   map[string]*user
-	bdInfos map[string]BDInfo
+	bdInfos map[string]*BDInfo
 }
 
 type BDInfo struct {
@@ -45,48 +45,49 @@ func (h *userHandler) loadBD() {
 	_, err := os.Stat(bdFile)
 	if err == nil {
 		if file, err := os.ReadFile(bdFile); err == nil {
-			i := new(map[string]BDInfo)
+			i := new(map[string]*BDInfo)
 			json.Unmarshal(file, i)
-			infos := *i
-			h.bdInfos = infos
-			log.Println("load bds ", infos)
+			h.bdInfos = *i
+			log.Println("load bds ", h.bdInfos)
 		}
 	}
 }
 
 func (h *userHandler) Recv(ctx *openwechat.MessageContext) {
 	if ctx.IsText() {
+		log.Printf("recv friend message = %s", ctx.Content)
 
 		id := ""
 		if sender, err := ctx.Sender(); err == nil {
 			friend, _ := sender.AsFriend()
 			id = friend.PYQuanPin + friend.RemarkPYQuanPin
 			h.assemblyUser(id, friend)
-
 		}
 
 		if flag := h.command(id, ctx.Content, func(msg string) { ctx.ReplyText(msg) }); flag {
 			return
 		}
 
+		if h.bdInfos[id].GptEnable {
+			ask := agent.AskAgent().Ask(id, ctx.Content)
+			if _, err := ctx.ReplyText(ask); err != nil {
+				log.Println("reply text error ", err)
+			}
+		}
 	}
 }
 
 func (h *userHandler) command(id, content string, f func(msg string)) bool {
 	if strings.Contains(content, "#gpt-enable#") {
 		h.users[id].gptEnable = true
-		info := h.bdInfos[id]
-		info.GptEnable = true
-		h.bdInfos[id] = info
-		f(h.ToString())
+		h.bdInfos[id].GptEnable = true
+		f(h.ToString(id))
 		return true
 	}
 	if strings.Contains(content, "#gpt-disable#") {
 		h.users[id].gptEnable = false
-		info := h.bdInfos[id]
-		info.GptEnable = false
-		h.bdInfos[id] = info
-		f(h.ToString())
+		h.bdInfos[id].GptEnable = false
+		f(h.ToString(id))
 		return true
 	}
 	//todo 2023/6/8 lamkeizyi -
@@ -105,23 +106,33 @@ func (h *userHandler) assemblyUser(id string, friend *openwechat.Friend) {
 
 	//如果从本地临时文件获取到配置，则采用文件中的配置
 	info := h.bdInfos[id]
-	if len(info.Id) > 0 {
+	if info != nil {
 		u2.gptEnable = info.GptEnable
 	} else {
-		bdInfo := BDInfo{id, friend.NickName, false}
+		bdInfo := &BDInfo{id, friend.NickName, false}
 		h.bdInfos[id] = bdInfo
 	}
 }
 
-func (h *userHandler) ToString() string {
-	//todo 2023/6/8 lamkeizyi -
-
-	return ""
+func (h *userHandler) ToString(id string) string {
+	var result = ""
+	result += "======setting======\n"
+	if h.bdInfos[id].GptEnable {
+		result += "gpt-enable = true \n"
+	}
+	if !h.bdInfos[id].GptEnable {
+		result += "gpt-enable = false \n"
+	}
+	result += "======setting======"
+	return result
 }
 
 func CreateUserHandler() *userHandler {
 	if loader.LoadBool("user.enable") {
-		u := &userHandler{}
+		u := &userHandler{
+			make(map[string]*user),
+			make(map[string]*BDInfo),
+		}
 		u.loadBD()
 		users = u
 		return u
