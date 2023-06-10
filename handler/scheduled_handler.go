7a4242b37269564812b96d0cf3ceb0e1715b0918
Alex <467146659@qq.com>
2023-06-10 21:28:28 +0800

    fix: isWeekday

--- handler/scheduled_handler.go
+++ handler/scheduled_handler.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	loader "sf-bot/handler/load"
 	"sf-bot/handler/service"
+	"time"
 )
 
 // StartGroupMorningPaperTimer 2023/6/6 lamkeizyi - 工作日9点半运行
@@ -14,22 +15,26 @@ func StartGroupMorningPaperTimer() {
 		fmt.Println("initialize 9:30 timer")
 		c := cron.New()
 		c.AddFunc("1 30 9 ? * 2,3,4,5,6", func() {
-			if groupHandler.morningPaperMode == TEXT {
-				if dayTextService, err := service.GetPicDayTextService(); err == nil {
-					log.Println("cuz has error that send pic . convert to text to sending... ")
-					groupHandler.sendText(dayTextService.ToString())
-				}
-			} else {
-				service.StartPicDayService(func(name string) {
-					if err := groupHandler.sendPic(name); err != nil {
-						//convert to text
-						if dayTextService, err := service.GetPicDayTextService(); err == nil {
-							log.Println("cuz has error that send pic . convert to text to sending... ")
-							groupHandler.sendText(dayTextService.ToString())
-						}
 
+			// 2023/6/10 lamkeizyi - 是否是此cron有问题，独立加上工作日判断
+			if isWeekday(time.Now()) {
+				if groupHandler.morningPaperMode == TEXT {
+					if dayTextService, err := service.GetPicDayTextService(); err == nil {
+						log.Println("cuz has error that send pic . convert to text to sending... ")
+						groupHandler.sendText(dayTextService.ToString())
 					}
-				})
+				} else {
+					service.StartPicDayService(func(name string) {
+						if err := groupHandler.sendPic(name); err != nil {
+							//convert to text
+							if dayTextService, err := service.GetPicDayTextService(); err == nil {
+								log.Println("cuz has error that send pic . convert to text to sending... ")
+								groupHandler.sendText(dayTextService.ToString())
+							}
+
+						}
+					})
+				}
 			}
 		})
 		c.Start()
@@ -43,11 +48,21 @@ func StartGroupMoyuTimer() {
 		fmt.Println("initialize 10:00 timer")
 		c := cron.New()
 		c.AddFunc("1 0 10 ? * 2,3,4,5,6", func() {
-			service.StartMoyuPicDayService(func(name string) {
-				groupHandler.sendPic(name)
-			})
+
+			// 2023/6/10 lamkeizyi - 是否是此cron有问题，独立加上工作日判断
+			if isWeekday(time.Now()) {
+				service.StartMoyuPicDayService(func(name string) {
+					groupHandler.sendPic(name)
+				})
+			}
 		})
 		c.Start()
 		select {}
 	}
+}
+
+// 2023/6/10 lamkeizyi - 工作日判断
+func isWeekday(t time.Time) bool {
+	weekday := t.Weekday()
+	return weekday != time.Saturday && weekday != time.Sunday
 }
